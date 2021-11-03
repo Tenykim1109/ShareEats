bc160c6102db889350ba0587f5957b1b22ff9067
JosephNaa <js.pekah@gmail.com>
2021-11-03 21:32:28 +0900

    create 회원정보 수정

--- app/src/main/java/com/sharewanted/shareeats/src/main/mypage/edituser/EditUserActivity.kt
+++ app/src/main/java/com/sharewanted/shareeats/src/main/mypage/edituser/EditUserActivity.kt
@@ -1,8 +1,10 @@
 package com.sharewanted.shareeats.src.main.mypage.edituser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sharewanted.shareeats.databinding.ActivityEditUserBinding
+import com.sharewanted.shareeats.src.main.userlogin.LoginActivity
 
 private const val TAG = "EditUserActivity_싸피"
 class EditUserActivity : AppCompatActivity() {
@@ -12,5 +14,29 @@ class EditUserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.activityEditUserBtnBack.setOnClickListener { finish() }
+
+        binding.activityEditUserIvUser.setOnClickListener {
+            // 프사 변경
+        }
+
+        binding.activityEditUserTvLogout.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.activityEditUserTvWithdrawal.setOnClickListener {
+            // delete user
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.activityEditUserBtnEdit.setOnClickListener {
+            // update user
+
+            finish()
+        }
     }
 }
