c45454e1904b321205f894d111504d124bca8bb0
JosephNaa <js.pekah@gmail.com>
2021-11-06 15:36:48 +0900

    2021/11/06 신용카드 폼 작성 --- 나요셉

--- app/src/main/java/com/sharewanted/shareeats/src/main/home/participate/ParticipateActivity.kt
+++ app/src/main/java/com/sharewanted/shareeats/src/main/home/participate/ParticipateActivity.kt
@@ -3,12 +3,17 @@ package com.sharewanted.shareeats.src.main.home.participate
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sharewanted.shareeats.R
 import com.sharewanted.shareeats.databinding.ActivityParticipateBinding
 import com.sharewanted.shareeats.src.main.MainActivity
 
+private const val TAG = "ParticipateActivity_싸피"
 class ParticipateActivity : AppCompatActivity() {
     private lateinit var binding: ActivityParticipateBinding
     private lateinit var adapter: ParticipateAdapter
@@ -33,6 +38,22 @@ class ParticipateActivity : AppCompatActivity() {
             Toast.makeText(this, "걸제하기", Toast.LENGTH_SHORT).show()
 //            val intent = Intent(this, MainActivity::class.java)
 //            startActivity(intent)
+        }
+
+        var cardList = resources.getStringArray(R.array.credit_card)
+
+        val cardAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, cardList)
+
+        binding.activityParticipateSpinner.adapter = cardAdapter
+
+        binding.activityParticipateSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(this@ParticipateActivity, "${cardList[position]}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
         }
     }
 
