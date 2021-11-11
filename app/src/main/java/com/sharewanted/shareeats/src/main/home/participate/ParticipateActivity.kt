8a43eb0227706dc0be1bb4e7bff0c1a85865c8b1
JosephNaa <js.pekah@gmail.com>
2021-11-11 22:58:33 +0900

    2021/11/11 결제 후 홈화면으로 이동 --- 나요셉

--- app/src/main/java/com/sharewanted/shareeats/src/main/home/participate/ParticipateActivity.kt
+++ app/src/main/java/com/sharewanted/shareeats/src/main/home/participate/ParticipateActivity.kt
@@ -122,7 +122,10 @@ class ParticipateActivity : AppCompatActivity() {
                                 dataInputFlag = true
 
                                 Toast.makeText(this@ParticipateActivity, "글 작성완료", Toast.LENGTH_SHORT).show()
-                                startActivity(Intent(this@ParticipateActivity, HomeFragment::class.java))
+                                val intent = Intent(this@ParticipateActivity, MainActivity::class.java)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent)
                                 finish()
                             }
 
