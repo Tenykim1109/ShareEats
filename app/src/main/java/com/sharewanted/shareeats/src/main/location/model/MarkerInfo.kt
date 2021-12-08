a4fb135ac2785eecfb931a1d1ad1435e8b1a6a5d
Minjeong Kim <lona573@gmail.com>
2021-12-08 21:30:51 +0900

    2021/12/08 지도 오류 수정 --- 김민정
    <개발내용>
    
    1.  커스텀 마커에 게시글 정보가 제대로 표시되지 않는 버그 수정

--- app/src/main/java/com/sharewanted/shareeats/src/main/location/model/MarkerInfo.kt
+++ app/src/main/java/com/sharewanted/shareeats/src/main/location/model/MarkerInfo.kt
@@ -2,6 +2,6 @@ package com.sharewanted.shareeats.src.main.location.model
 
 import com.naver.maps.map.overlay.Marker
 
-data class MarkerInfo(val marker: Marker, val storeName: String, val title: String) {
+data class MarkerInfo(val marker: Marker, val title: String, val storeName: String) {
     constructor(): this(Marker(), "", "")
 }
